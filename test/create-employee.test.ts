51d1a973f320cb6bc11506667b411bd8f4f142b9
jorge <jorge@nursa.com>
2022-07-03 20:45:48 -0300

    fix: test typing

--- test/create-employee.test.ts
+++ test/create-employee.test.ts
@@ -11,7 +11,7 @@ describe("Create employee", () => {
     expect(employee).toBeDefined();
   });
 
-  it("Should throw and error if age are less than 12 years", () => {
+  it("should throw and error if age are less than 12 years", () => {
     expect(
       () => new Employee("123", "Jorge", new Date("2020-01-10"), "any")
     ).toThrow(new Error("Age must be greater than 12 years"));
