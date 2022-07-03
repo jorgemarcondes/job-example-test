d2edfe401650f5b5c1baa6b5e65fdd48ada60e20
jorge <jorge@nursa.com>
2022-07-03 20:44:48 -0300

    feat: add job status

--- src/job.ts
+++ src/job.ts
@@ -1,13 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+import { JobStatus } from "./job-status.enum";
+
 export default class Job {
+  readonly id: string;
+  private price: number = 0;
+  readonly status: JobStatus;
+
   constructor(
-    readonly id: string,
-    private title: string,
-    private description: string,
-    private start: Date,
-    private end: Date,
-    private price: number
+    readonly companyId: string,
+    readonly title: string,
+    readonly description: string,
+    readonly start: Date,
+    readonly end: Date
   ) {
     if (start >= end) throw new Error("Start have to be greater than end");
-    if (price <= 0) throw new Error("Price must be greater than zero");
+
+    this.id = uuidv4();
+    this.status = JobStatus.CREATED;
   }
 }
--- test/create-job.test.ts
+++ test/create-job.test.ts
@@ -1,54 +1,51 @@
 import Job from "../src/job";
+import { JobStatus } from "../src/job-status.enum";
 
-describe("Create job test", () => {
-  it("Have to be created", () => {
+describe("Create job", () => {
+  it("should to be created", async () => {
     const job = new Job(
       "123",
       "Developer",
       "Create an authentication module",
       new Date("2022-01-10T10:00:00"),
-      new Date("2022-01-10T19:00:00"),
-      50
+      new Date("2022-01-10T19:00:00")
     );
-    expect(job).toBeDefined();
+
+    expect(job.id).toBeDefined();
   });
 
-  it("Should throw an error if start be greater than end", () => {
+  it("should have start greater than end", () => {
     expect(() => {
       new Job(
         "123",
         "Developer",
         "Create an authentication module",
         new Date("2022-01-10T17:00:00"),
-        new Date("2022-01-10T10:00:00"),
-        50
+        new Date("2022-01-10T10:00:00")
       );
     }).toThrow(new Error("Start have to be greater than end"));
   });
 
-  it("Should throw an error if start equal to end", () => {
+  it("should have start not equal to end", () => {
     expect(() => {
       new Job(
         "123",
         "Developer",
         "Create an authentication module",
         new Date("2022-01-10T17:00:00"),
-        new Date("2022-01-10T17:00:00"),
-        50
+        new Date("2022-01-10T17:00:00")
       );
     }).toThrow(new Error("Start have to be greater than end"));
   });
 
-  it("Should throw an error if price is less or equal zero", () => {
-    expect(() => {
-      new Job(
-        "123",
-        "Developer",
-        "Create an authentication module",
-        new Date("2022-01-10T10:00:00"),
-        new Date("2022-01-10T18:00:00"),
-        0
-      );
-    }).toThrow(new Error("Price must be greater than zero"));
+  it("should create job with status CREATED", () => {
+    const job = new Job(
+      "123",
+      "Developer",
+      "Create an authentication module",
+      new Date("2022-01-10T10:00:00"),
+      new Date("2022-01-10T17:00:00")
+    );
+    expect(job.status).toBe(JobStatus.CREATED);
   });
 });
