4ed7a94af799f0d85641fd98a912bdd5c679e316
jorge <jorge@nursa.com>
2022-07-03 20:45:59 -0300

    fix: job request

---
+++ src/job-request.ts
@@ -0,0 +1,18 @@
+import { JobStatus } from "./job-status.enum";
+
+export default class JobRequest {
+  async execute(input: Input): Promise<Output> {
+    return {
+      status: JobStatus.REQUESTED,
+    };
+  }
+}
+
+type Input = {
+  employeeId: string;
+  jobId: string;
+};
+
+type Output = {
+  status: JobStatus;
+};
